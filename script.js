ee1b1da70e4170bfb255d385654d16895cc27f2d
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-11 22:36:12 +0530

    expression is valid or not

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 // Tricky JS Program
 console.clear();
 
-// How to determine string is unique or not in JavaScript
+// expression is valid or not in JavaScript
 
-function unique(str) {
-    let values = [];
-    for (let letter of str) {
-        if (values.indexOf(letter) !== -1) {
-            return false // Represent string is unique 
+let expression_01 = '(a)';
+let expression_02 = '((b))';
+let expression_03 = '(a(b))';
+let expression_04 = '(b)';
+let expression_05 = '(b';
+
+const is_valid = (expression) => {
+    let characters = expression.split("");
+    let stack = [];
+    for (let i = 0; i < characters.length; i++) {
+        let c = characters[i];
+        if (c !== '(' && c !== ')') continue;
+        else if (c === '(') stack.push(c);
+        else if (c === ')') {
+            if (stack.length === 0) return false;
+            else stack.pop();
         }
-        values.push(letter);
     }
-    return true;
-}
+    return stack.length === 0;
+};
 
-console.log(unique('abcdef')); // true
-console.log(unique('abadea')); // false
+let one = is_valid(expression_01) ? `${expression_01} currect expression` : `${expression_01} incurrect expression`;
+let two = is_valid(expression_02) ? `${expression_02} currect expression` : `${expression_02} incurrect expression`;
+let three = is_valid(expression_03) ? `${expression_03} currect expression` : `${expression_03} incurrect expression`;
+let four = is_valid(expression_04) ? `${expression_04} currect expression` : `${expression_04} incurrect expression`;
+let five = is_valid(expression_05) ? `${expression_05} currect expression` : `${expression_05} incurrect expression`;
+
+console.log(one); // currect expression
+console.log(two); // currect expression
+console.log(three); // currect expression
+console.log(four); // currect expression
+console.log(five); // incurrect expression
