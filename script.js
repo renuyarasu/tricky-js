babd873b55d43162011ca38a888ace260cc78172
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-11 22:19:26 +0530

    determine String is unique

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Tricky JS Program
+console.clear();
+
+// How to determine string is unique or not in JavaScript
+
+function unique(str) {
+    let values = [];
+    for (let letter of str) {
+        if (values.indexOf(letter) !== -1) {
+            return false // Represent string is unique 
+        }
+        values.push(letter);
+    }
+    return true;
+}
+
+console.log(unique('abcdef')); // true
+console.log(unique('abadea')); // false
