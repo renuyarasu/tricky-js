0a3d88f9dcc14b7b1ec5b018d6429363590dbba9
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-12 17:56:57 +0530

    Shuffle Array

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 // Tricky JS Program
 console.clear();
 
-// Understand BigInt in JavaScript ES2020-21
+// 7 Killer One-Liners in JavaScript
+// Shuffle Array
+const shuffle_array = (arr) => arr.sort(() => Math.random() - 0.5);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-let num = Number.MAX_SAFE_INTEGER;
-
-console.log('Number_01: ', num); // 9007199254740991
-console.log('Number_02: ', ++num); // 9007199254740992
-console.log('Number_03: ', ++num); // 9007199254740992
-console.log('Number_04: ', ++num); // 9007199254740992
-console.log('Number_05: ', ++num); // 9007199254740992
-
-let new_num = 9007199254740992n;
-
-console.log('New Number_01: ', new_num); // 9007199254740992n
-console.log('New Number_02: ', ++new_num); // 9007199254740993n
-console.log('New Number_03: ', ++new_num); // 9007199254740994n
-console.log('New Number_04: ', ++new_num); // 9007199254740995n
-console.log('New Number_05: ', ++new_num); // 9007199254740996n
+let result = shuffle_array(arr);
+console.log(result);
